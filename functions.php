1727645eb0293f0b6afe442df7d162540eabc3b1
Ale <akak@hotmail.it>
2017-12-11 17:42:04 +0100

    Implement first custom post type - Events. Added labeling parameters for Events in admin page....

--- functions.php
+++ functions.php
@@ -8,16 +8,17 @@
 		wp_enqueue_style("roboto-font", "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
 	}
 
+	add_action("wp_enqueue_scripts", "university_files");
+
 	function university_features()
 	{
-		//register menus to be edited in admin interface and used in html pages
+		// implement this to register menus to be edited in admin interface and used in html pages
 		//register_nav_menu("headerMenuLocation", "Header menu location");
 		
 		//dynamic page titling...
 		add_theme_support("title-tag");
 	}
 
-	add_action("wp_enqueue_scripts", "university_files");
 	add_action("after_setup_theme", "university_features");
 
-?>
+
