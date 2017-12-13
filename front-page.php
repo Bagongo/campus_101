272669d0fcc44a326ae09dcc0b7224351202dd4c
Ale <akak@hotmail.it>
2017-12-13 18:36:48 +0100

    Added advanced custom fields plugin. Added custom field 'event_date' to event post typr. Implented display of event date to front page events. Added custom query to manage number of events to display and relative sorting

--- front-page.php
+++ front-page.php
@@ -16,16 +16,26 @@
         <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
         
         <?php 
-          $homepageEvents = new WP_Query(array("posts_per_page" => 2,
-            "post_type" => "event"));
+          $currentDay = date("Ymd");
+          $homepageEvents = new WP_Query(array(
+                                "posts_per_page" => 2,
+                                "post_type" => "event",
+                                "meta_key" => "event_date",
+                                "order_by" => "meta_value_num",
+                                "order" => "DESC",
+                                "meta_query" => array(array("key" => "event_date", "compare" => ">=", "value" => $currentDay), "type" => "numeric")
+                              ));
 
             while($homepageEvents->have_posts()) : $homepageEvents->the_post();           
         ?>
 
           <div class="event-summary">
             <a class="event-summary__date t-center" href="">
-              <span class="event-summary__month"><?php the_time("M"); ?></span>
-              <span class="event-summary__day"><?php the_time("d"); ?></span>  
+              <span class="event-summary__month"><?php 
+                $eventDate = new DateTime(get_field("event_date"));
+                echo $eventDate->format('M');
+               ?></span>
+              <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>  
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
