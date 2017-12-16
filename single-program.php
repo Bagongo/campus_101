a1de4aa248c0a705c72f78318446230659afd7d0
Ale <akak@hotmail.it>
2017-12-16 19:30:57 +0100

    Added view rendering of relation between events & programs (for both sides)

--- single-program.php
+++ single-program.php
@@ -1,7 +1,9 @@
 <?php
 	get_header();
 
-	while(have_posts()) { the_post(); ?>
+	while(have_posts()) : 
+	the_post(); 
+?>
 		<div class="page-banner">
 		  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("/images/ocean.jpg") ?>);"></div>
 		  <div class="page-banner__content container container--narrow">
@@ -24,9 +26,58 @@
 			</div>
 			<div class="generic-content"><?php the_content() ?></div>
 
+			<?php 
+			  $currentDay = date("Ymd");
+			  $homepageEvents = new WP_Query(array(
+			                        "posts_per_page" => -1,
+			                        "post_type" => "event",
+			                        "meta_key" => "event_date",
+			                        "order_by" => "meta_value_num",
+			                        "order" => "DESC",
+			                        "meta_query" => array(
+			                        	array(
+			                        		"key" => "event_date", 
+			                        		"compare" => ">=",
+			                        		"value" => $currentDay,
+			                        		"type" => "numeric"
+			                        	), 
+				                        array(
+				                        	'key' => 'related_programs',
+				                        	'compare' => 'LIKE', 
+				                        	'value' => '"' . get_the_ID() . '"'
+				                        )
+			                      )
+			                    )
+			);
+
+			    while($homepageEvents->have_posts()) : 
+			    $homepageEvents->the_post();           
+			?>
+
+			  <div class="event-summary">
+			    <a class="event-summary__date t-center" href="">
+			      <span class="event-summary__month"><?php 
+			        $eventDate = new DateTime(get_field("event_date"));
+			        echo $eventDate->format('M');
+			       ?></span>
+			      <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>  
+			    </a>
+			    <div class="event-summary__content">
+			      <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
+			      <p><?php if(has_excerpt()) 
+			                    echo get_the_excerpt();
+			                else echo wp_trim_words(get_the_content(), 15); ?> 
+			        <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a>
+			      </p>
+			    </div>
+			  </div>
+
+			<?php endwhile ?>
+
 		</div>
 
-	<?php }
-	 
-	get_footer();
-?>
+	<?php
+		endwhile;
+		get_footer();
+	?>
+
