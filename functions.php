27a312b455d6bd2e351a6e070e45fa5139858545
Ale <akak@hotmail.it>
2017-12-27 16:34:59 +0100

    Setup banner function covering title subtilte and bg image...

--- functions.php
+++ functions.php
@@ -20,6 +20,7 @@
 		add_theme_support("post-thumbnails");
 		add_image_size("professor-landscape", "400", "260", true);
 		add_image_size("professor-portrait", "480", "650", true);
+		add_image_size("page-banner", "1500", "350", true);
 	}
 
 	add_action("after_setup_theme", "university_features");
@@ -46,5 +47,44 @@
 	}
 
 	add_action("pre_get_posts", "university_adjust_queries");
+
+	//custom function to clearly print out vars (var dumping)
+	function vd($var) 
+	{
+	    echo "<pre>";
+	    print_r($var);
+	    echo "</pre>";
+	}
+
+	function pageBanner($args = null)
+	{
+		if(!$args["title"])
+			$args["title"] = get_the_title();
+
+		if(!$args["subtitle"])
+			$args["subtitle"] = get_field("page_banner_subtitle");
+
+		if(!$args["photo"])
+		{
+			if(get_field("page_banner_background_image"))
+				$args["photo"] = get_field("page_banner_background_image")["sizes"]["page-banner"];
+			else
+			$args["photo"] = get_theme_file_uri("/images/ocean.jpg");
+		}
+		
+		?>
+
+		<div class="page-banner">
+		  <div class="page-banner__bg-image" style="background-image: url(<?php echo $args["photo"]; ?>);"></div>
+		  <div class="page-banner__content container container--narrow">
+		    <h1 class="page-banner__title"><?php echo $args["title"]; ?></h1>
+		    <div class="page-banner__intro">
+		      <p><?php echo $args["subtitle"]; ?></p>
+		    </div>
+		  </div>  
+		</div>
+		
+	<?php  
+	}
 
 
