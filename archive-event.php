ab55f1414230def9ae033e7975bcfc60809a263d
Ale <akak@hotmail.it>
2017-12-27 18:52:38 +0100

    Substitution of hardcoded pagebanner in all pages with proper function

--- archive-event.php
+++ archive-event.php
@@ -1,14 +1,11 @@
-<?php get_header() ?>
+<?php 
+	get_header();
+	pageBanner(array(
+		"title" => "All events",
+		"subtitle" => "See what's happening...."
+	));
 
-<div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("/images/ocean.jpg") ?>);"></div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">Current events</h1>
-    <div class="page-banner__intro">
-      <p>Check out our current events!</p>
-    </div>
-  </div>  
-</div>
+ ?>
 
 <div class="container container--narrow page-section">
 	<?php while(have_posts()) :
