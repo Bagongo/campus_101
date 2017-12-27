2d5ab85f169601901798ab1f5cbc31f770225d1a
Ale <akak@hotmail.it>
2017-12-27 20:15:41 +0100

    Code refactoring: Added event.php partial to load events rendering in all relevant views

--- archive-event.php
+++ archive-event.php
@@ -8,25 +8,13 @@
  ?>
 
 <div class="container container--narrow page-section">
-	<?php while(have_posts()) :
-		the_post();
-	?>
-
-	<div class="event-summary">
-		<a class="event-summary__date t-center" href="">
-	      <span class="event-summary__month"><?php 
-	        $eventDate = new DateTime(get_field("event_date"));
-	        echo $eventDate->format('M');
-	       ?></span>
-	      <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>  
-	    </a>
-		<div class="event-summary__content">
-		  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
-		  <p><?php the_excerpt(); ?> <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
-		</div>
-	</div>
-
-	<?php endwhile ?> 
+	<?php 
+		while(have_posts())
+		{
+			the_post();
+			get_template_part("template-parts/content-event");
+		} 
+	?> 
 	<?php echo paginate_links() ?>
 
 	<p><b>Wanna see some events you missed?</b><a href="<?php echo site_url("/past-events") ?>"> >> Go to past events...</a></p>
--- page-past-events.php
+++ page-past-events.php
@@ -19,28 +19,17 @@
 		                                	"compare" => "<", "value" => $currentDay), "type" => "numeric")
 		                            ));
 
-		while($pastEvents->have_posts()) : 
-		$pastEvents->the_post();
-	?>
+		while($pastEvents->have_posts())
+		{ 
+			$pastEvents->the_post();
+			get_template_part("template-parts/content-event");
 
-	<div class="event-summary">
-		<a class="event-summary__date t-center" href="">
-	      <span class="event-summary__month"><?php 
-	        $eventDate = new DateTime(get_field("event_date"));
-	        echo $eventDate->format('M');
-	       ?></span>
-	      <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>  
-	    </a>
-		<div class="event-summary__content">
-		  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
-		  <p><?php the_excerpt(); ?> <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
-		</div>
-	</div>
+		}	
 
-	<?php endwhile ?> 
-	<?php echo paginate_links(array(
+		echo paginate_links(array(
 			"total" => $pastEvents->max_num_pages
-		)); ?>
+		)); 
+	?>
 
 </div>
 
