a6199153d9b875efaa2cfd84de0ff12fdc679374
Ale <akak@hotmail.it>
2018-01-07 17:26:11 +0100

    Added campus-archive. Implemented maps and pins via js with google maps api. Updated campus link in header.

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 	function university_files()
 	{
+		wp_enqueue_script("google_map", "//maps.googleapis.com/maps/api/js?key=AIzaSyBq679gUNs1FEqyWBvBT47i6B5bFaDyMio", NULL, "1.0", true);
 		wp_enqueue_script("university_main_js", get_theme_file_uri("/js/scripts-bundled.js"), NULL, "1.0", true);
 		wp_enqueue_style("university_main_stiles", get_stylesheet_uri());
 		wp_enqueue_style("font-awesome", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
