82152eaa62d99e458687b9befa8d2eed31e57702
Ale <akak@hotmail.it>
2018-01-07 19:30:16 +0100

    Added single campus view. Created relation program -> campus (one to many). Implemented relations in single program and single campus

--- archive-campus.php
+++ archive-campus.php
@@ -14,10 +14,12 @@
 			$mapLocation = get_field("map_location");
 		?>
 			
-		<div class="marker" data-lat="<?php echo $mapLocation["lat"]; ?>" data-lng="<?php echo $mapLocation["lng"]; ?>"></div>
+		<div class="marker" data-lat="<?php echo $mapLocation["lat"]; ?>" data-lng="<?php echo $mapLocation["lng"]; ?>">
+			<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			<?php echo $mapLocation["address"]; ?>
+		</div>
 
 		<?php endwhile ?> 
-		<?php echo paginate_links() ?>
 	</div>
 
 </div>
--- single-campus.php
+++ single-campus.php
@@ -10,25 +10,35 @@
 
 		<div class="metabox metabox--position-up metabox--with-home-link">
 			<p>
-				<a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link("program"); ?>">
-					<i class="fa fa-home" aria-hidden="true"></i> All programs 
+				<a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link("campus"); ?>">
+					<i class="fa fa-home" aria-hidden="true"></i> All campuses 
 				</a> 
 				<span class="metabox__main"><?php the_title() ?></span>
 			</p>
 		</div>
 		<div class="generic-content"><?php the_content() ?></div>
 
+		<div class="acf-map">
+
+			<?php $mapLocation = get_field("map_location"); ?>
+			
+			<div class="marker" data-lat="<?php echo $mapLocation["lat"]; ?>" data-lng="<?php echo $mapLocation["lng"]; ?>">
+				<h3><?php the_title(); ?></h3>
+				<?php echo $mapLocation["address"]; ?>
+			</div>
+
+		</div>
+
 		<?php 
 
-
-		  $relatedProfessors = new WP_Query(array(
+		  $relatedPrograms = new WP_Query(array(
 		                        "posts_per_page" => -1,
-		                        "post_type" => "professor",
+		                        "post_type" => "program",
 		                        "order_by" => "title",
 		                        "order" => "ASC",
 		                        "meta_query" => array(
 			                        array(
-			                        	'key' => 'related_programs',
+			                        	'key' => 'related_campus',
 			                        	'compare' => 'LIKE', 
 			                        	'value' => '"' . get_the_ID() . '"'
 			                        )
@@ -36,21 +46,19 @@
 		                    )
 			);
 
-			if($relatedProfessors->have_posts())
-			{  
+			if($relatedPrograms->have_posts())
+			{ 
+				$campusName = get_the_title(); 
 
 			  	echo "<hr class='section-break'>";
-			  	echo "<h2 class='headline headline--medium'>" . get_the_title() . " Professors</h2>";
+			  	echo "<h2 class='headline headline--medium'>Programs available at '{$campusName}'</h2>";
 
-			  	echo "<ul>";
-			    while($relatedProfessors->have_posts()) : 
-				    $relatedProfessors->the_post();           
+			  	echo "<ul class='min-list link-list'>";
+			    while($relatedPrograms->have_posts()) : 
+				    $relatedPrograms->the_post();           
 				?>
-				<li class="professor-card__list-item">
-					<a class="professor-card" href="<?php the_permalink(); ?>">
-						<img class="professor-card__image" src="<?php the_post_thumbnail_url('professor-landscape'); ?>" />
-						<span class="professor-card__name"><?php the_title(); ?></span>
-					</a>
+				<li>
+					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 				</li>
 
 			<?php 
@@ -95,24 +103,6 @@
 			    get_template_part("template-parts/content-event");           			  
 			}
 		} 
-
-		wp_reset_postdata();
-		$relatedCampuses = get_field("related_campus");
-
-		if($relatedCampuses)
-		{
-			echo "<hr class='section-break'>";
-			echo "<h2 class='headline headline--medium'>" . get_the_title() . " is available at these campuses: </h2>";
-
-			echo "<ul>";
-			foreach ($relatedCampuses as $campus) 
-			{
-				?> <li><a href="<?php get_the_permalink($campus); ?>"><?php echo get_the_title($campus); ?></a></li> <?php 
-			}
-			echo "</ul>";
-		}
-
-
 	?>
 
 		</div>
