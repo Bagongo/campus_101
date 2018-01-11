ab2e20ca9bd85af346958572ae7417d0ef066df1
Ale <akak@hotmail.it>
2018-01-11 17:01:05 +0100

    Set custom search logic to search whole database and fill categories in properly structured json

--- inc/search-route.php
+++ inc/search-route.php
@@ -10,7 +10,66 @@
 		));
 	}
 
-	function universitySearchResults()
+	function universitySearchResults($data)
 	{
-		return "This is a new route";
+		$mainQuery = new WP_Query(array(
+			"post_type" => array("post", "page", "professor", "program", "campus", "event"),
+			"s" => sanitize_text_field($data["term"]) 
+		));
+
+		$results = array(
+			"generalInfo" => array(),
+			"professors" => array(),
+			"programs" => array(),
+			"events" => array(),
+			"campuses" => array()
+
+		);
+
+		while($mainQuery->have_posts())
+		{
+			$mainQuery->the_post();
+
+			if(get_post_type() == "post" OR get_post_type() == "page")
+			{
+				array_push($results["generalInfo"], array(
+					"title" => get_the_title(),
+					"permalink" => get_the_permalink()
+				));
+			}
+
+			if(get_post_type() == "professor")
+			{
+				array_push($results["professors"], array(
+					"title" => get_the_title(),
+					"permalink" => get_the_permalink()
+				));
+			}
+
+			if(get_post_type() == "program")
+			{
+				array_push($results["programs"], array(
+					"title" => get_the_title(),
+					"permalink" => get_the_permalink()
+				));
+			}
+
+			if(get_post_type() == "event")
+			{
+				array_push($results["events"], array(
+					"title" => get_the_title(),
+					"permalink" => get_the_permalink()
+				));
+			}
+
+			if(get_post_type() == "campus")
+			{
+				array_push($results["campuses"], array(
+					"title" => get_the_title(),
+					"permalink" => get_the_permalink()
+				));
+			}
+		}
+
+		return $results;
 	}
