cf1a8e278092ca4aafee78152a12f7270a264c31
Ale <akak@hotmail.it>
2018-01-14 16:07:01 +0100

    Added queries and filters for relations in main search. Search logic complete....

--- inc/search-route.php
+++ inc/search-route.php
@@ -23,7 +23,6 @@
 			"programs" => array(),
 			"events" => array(),
 			"campuses" => array()
-
 		);
 
 		while($mainQuery->have_posts())
@@ -51,9 +50,23 @@
 
 			if(get_post_type() == "program")
 			{
+				$relatedCampuses = get_field("related_campus");
+
+				if($relatedCampuses)
+				{
+					foreach ($relatedCampuses as $campus) 
+					{
+						array_push($results["campuses"], array(
+							"title" => get_the_title($campus),
+							"permalink" => get_the_permalink($campus)
+						));
+					}
+				}
+
 				array_push($results["programs"], array(
 					"title" => get_the_title(),
-					"permalink" => get_the_permalink()
+					"permalink" => get_the_permalink(),
+					"id" => get_the_id()
 				));
 			}
 
@@ -83,6 +96,62 @@
 					"permalink" => get_the_permalink()
 				));
 			}
+		}
+
+		if($results["programs"])
+		{
+			$programsMetaquery = array("relation" => "OR");
+
+			foreach ($results["programs"] as $item)
+			{
+				array_push($programsMetaquery, array(
+						"key" => "related_programs",
+						"compare" => "LIKE",
+						"value" => '"' . $item["id"] . '"'
+					));
+			}
+
+			$programRelationship = new WP_Query(array(
+				"post_type" => array("professor", "event"),
+				"meta_query" => $programsMetaquery
+			));
+
+			while($programRelationship->have_posts())
+			{
+				$programRelationship->the_post();
+
+				if(get_post_type() == "event")
+				{
+					$eventDate = new DateTime(get_field("event_date"));
+					$description = null;
+
+					if(has_excerpt()) 
+						$description = get_the_excerpt();
+					else 
+						$description = wp_trim_words(get_the_content(), 15);
+
+					array_push($results["events"], array(
+						"title" => get_the_title(),
+						"permalink" => get_the_permalink(),
+						"month" => $eventDate->format('M'),
+						"day" => $eventDate->format('d'),
+						"description" => $description
+					));
+				}
+
+				if(get_post_type() == "professor")
+				{
+					array_push($results["professors"], array(
+						"image" => get_the_post_thumbnail_url(0, 'professor-landscape'),
+						"title" => get_the_title(),
+						"permalink" => get_the_permalink()
+					));
+				}
+			}
+
+			$results["professors"] = array_values(array_unique($results["professors"], SORT_REGULAR));
+
+			$results["events"] = array_values(array_unique($results["events"], SORT_REGULAR));
 		}
 
 		return $results;
