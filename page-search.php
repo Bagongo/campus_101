291eca7f2b3f8ebfc6379db9ecf6a2bb5dc45781
Ale <akak@hotmail.it>
2018-01-16 17:06:18 +0100

    Added search.php ( result-specific page). Improved layout for results in non-js search and split in partials

--- page-search.php
+++ page-search.php
@@ -46,13 +46,7 @@
 		<?php endif ?>  
 
 		  <div class="generic-content">
-		  	<form class="search-form" method="get" action="<?php echo esc_url(site_url('/')) ?>">
-		  		<label class="headline headline--medium" for="s">Perform a search on this website</label>
-		  		<div class="search-form-row">
-			  		<input class="s" id="s" type="search" name="s" placeholder="What are you looking for....?">
-			  		<input class="submit"  type="submit" value="search">
-			  	</div>
-		  	</form>
+		  	<?php get_search_form(); ?>
 		  </div>
 
 		</div>
